32df76a55553d4f42a3b3004d76fad5b2f8d21bd
Evgeniy Vilkov <zzeneg@gmail.com>
2013-04-10 01:05:57 +0400

    Bug fixing + comments

--- TwVk.py
+++ TwVk.py
@@ -15,6 +15,7 @@ vkToken = "5de2747dc7f337856a4f9eaa1bb7ebe8f809f691b2bb6b04586734037da57a53e7540
 #EyeEm token
 eyeEmToken = "1d458eb10a2cc545790ff3748c2acff567fce124"
 
+#Main method
 def main():
     try:
         logging.basicConfig(format = '%(asctime)s.%(msecs)d %(levelname)s %(message)s',
@@ -23,86 +24,105 @@ def main():
                             level = logging.INFO)
         logging.info('Script started')
 
+        #authorization in twitter
         url = "https://userstream.twitter.com/1.1/user.json"
         param = {"delimited":"length", "with":"user"}
         header = {}
-
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_key, access_secret)
         auth.apply_auth(url, "POST", header,param)
 
         logging.info('Twitter authorization successful')
 
+        #getting tweets
         req = urllib2.Request(url)
         req.add_header("Authorization", header["Authorization"])
         r = urllib2.urlopen(req, urllib.urlencode(param), 90)
 
+        #reading responses
         while True:
             try:
-                len = ""
+                #get response length
+                length = ""
                 while True:
                     c = r.read(1)
-                    if c=="\n": break
-                    if c=="": raise Exception
-                    len += c
-                len = len.strip()
-                if not len.isdigit(): continue
-                t = json.loads(r.read(int(len)))
+                    if c == "\n": break
+                    if c == "": raise Exception
+                    length += c
+                length = length.strip()
+                if not length.isdigit(): continue
+                #read response with length
+                tweet = json.loads(r.read(int(length)))
 
-                if "user" in t and "text" in t and "created_at" in t and "id" in t:
-                    handleTweet(t)
+                #if it's a tweet - start handling it
+                if "user" in tweet and "text" in tweet and "created_at" in tweet and "id" in tweet:
+                    handleTweet(tweet)
             except:
                 logging.exception('Exception')
     except:
         logging.exception('Main exception')
 
+#Method handling tweet
 def handleTweet(tweet):
     logging.info('Starting handleTweet')
+    #get tweet text
     text = tweet["text"]
     logging.info('Tweet text: ' + text)
-    if not text.startswith('@') and not "@zzeneg" in text):
+    #check for answer, retweet or mention
+    if not text.startswith('@') and not "@zzeneg" in text:
+        #get urls in tweet
         urls = tweet["entities"]["urls"]
         eyeEmId = ""
+        attachments = ""
 
+        #replace shortened urls
         for url in urls:
             text = text.replace(url["url"], url["expanded_url"])
+            #if it's EyeEm photo - get it id
             if url["expanded_url"].startswith("http://www.eyeem.com/p/"):
                 eyeEmId = url["expanded_url"].replace("http://www.eyeem.com/p/", "")
                 logging.info('EyeEm Id: ' + eyeEmId)
-        logging.info('Tweet text with urls: ' + text)
-        attachments = ""
+                photoUrl = getEyeEmPhotoUrlById(eyeEmId)
+                logging.info('Photo found. Url: ' + photoUrl)
+                attachments = uploadPhoto(photoUrl)
 
+        logging.info('Tweet text with urls: ' + text)
+
+        #if photo is attached - remove it and upload to VK
         if "media" in tweet["entities"]:
             photo = tweet["entities"]["media"][0]
             logging.info('Photo found. Url: ' + photo["media_url"])
             text = text.replace(photo["url"], "")
             attachments = uploadPhoto(photo["media_url"])
 
-        if (eyeEmId != ""):
-            photoUrl = getEyeEmPhotoUrlById(eyeEmId)
-            logging.info('Photo found. Url: ' + photoUrl)
-            attachments = uploadPhoto(photoUrl)
-
         logging.info('Attachments: ' + attachments)
-        vkMethod('wall.post', {'message': text,'attachments':attachments})
+        #post to VK and attach photo ID
+        #vkMethod('wall.post', {'message': text,'attachments':attachments})
         logging.info('Posted to VK')
     else:
         logging.info('Skipping tweet')
 
+#Method uploading photo to VK
 def uploadPhoto(fileUrl):
     logging.info('Start uploading photo')
+    #get url for uploading photo
     response = vkMethod('photos.getWallUploadServer')
     uploadUrl = response['response']['upload_url']
     logging.info('Get upload url: ' + uploadUrl)
+    #saving photo locally
     urllib.urlretrieve(fileUrl,'temp.jpg')
     logging.info('Photo saved on local drive')
+    #uploading photo to VK
     files = {'photo': open('temp.jpg', 'rb')}
     response = requests.post(uploadUrl, files = files).json
     logging.info('Photo uploaded')
+    #add photo in wall album
     response = vkMethod('photos.saveWallPhoto', response)
     logging.info('Photo saved in wall album')
+    #return photo ID
     return response['response'][0]['id']
 
+#Common VK API method
 def vkMethod(method, data={}):
     url = 'https://api.vk.com/method/%s.json' % (method)
     data.update({'access_token': vkToken})
@@ -115,10 +135,10 @@ def vkMethod(method, data={}):
 
     return response
 
-def getEyeEmPhotoUrlById(id):
-    url = "https://www.eyeem.com/api/v2/photos/{0}?access_token={1}".format(id, eyeEmToken)
+#Method getting photo URL from EyeEm
+def getEyeEmPhotoUrlById(photoId):
+    url = "https://www.eyeem.com/api/v2/photos/{0}?access_token={1}".format(photoId, eyeEmToken)
     response = requests.get(url).json
     return response['photo']['photoUrl']
 
 main()
-
